cbd6963c2f444a3fe2a5cc1fbbbee6f42f8424a7
root <root@logoproblem.net>
2021-10-16 16:24:45 +0200

    configs

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=tuman_db',
-    'username' => 'zazir',
-    'password' => '0P9o8i7u@',
+    'dsn' => 'mysql:host=tuman_app-mysql;dbname=tuman_db',
+    'username' => 'root',
+    'password' => 'local',
     'charset' => 'utf8',
 ];
